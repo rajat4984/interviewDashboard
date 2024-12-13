412cb45ba7f760fee217b294d17064d09982e95b
rajat4984 <rs20021023@gmail.com>
2024-12-14 01:44:46 +0530

    navbar started

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,30 +1,32 @@
-import { Bell, Grid2x2, Mail, Search, Settings } from "lucide";
+import { Bell, Grid2x2, Mail, Search, Settings } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <Grid2x2 />
-        <div>
-          <Search />
-          <input placeholder="Search" />
+    <>
+      <nav className="bg-cstm-bg-light-gray flex items-center justify-between">
+        <div className="flex items-center">
+          <Grid2x2 color="#7290ff" strokeWidth={1.5} />
+          <div className="relative">
+            <Search color="#dedee2" size={15} className="absolute top-1/2 left-1 transform -translate-y-1/2" />
+            <input placeholder="Search" className="bg-[#2a2a31] pl-6" />
+          </div>
         </div>
-      </div>
-      <div>
-        <div>
-          <Mail />
+        <div className="flex items-center justify-between gap-5">
+          <div className="bg-icon-bg p-2 rounded-full">
+            <Mail color="#a1a3a7" size={18} />
+          </div>
+          <div className="bg-icon-bg p-2 rounded-full">
+            <Settings size={18} color="#a1a3a7" />
+          </div>
+          <div className="bg-icon-bg p-2 rounded-full">
+            <Bell size={18} color="#a1a3a7" />
+          </div>
+          <div className="w-10 h-10">
+            <img className="rounded-full" src="/images/profile_picture.jpg" />
+          </div>
         </div>
-        <div>
-          <Settings />
-        </div>
-        <div>
-          <Bell />
-        </div>
-        <div>
-
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "cstm-bg-light-gray": "#202028",
+        "icon-bg":"#46474c",
+      },
+    },
   },
   plugins: [],
 };
